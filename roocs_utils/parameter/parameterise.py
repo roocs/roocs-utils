8b132e6947b88f9750356db313bc6f1b70c8b1e3
Eleanor Smith <esmith88@jasmin-sci5.ceda.ac.uk>
2020-08-13 15:36:17 +0100

    fixed spelling of parameterise and added parameterise_rook

--- roocs_utils/parameter/parameterise.py
+++ roocs_utils/parameter/parameterise.py
@@ -6,7 +6,17 @@ from roocs_utils.parameter import (
 )
 
 
-def parametrise_daops(collection=None, area=None, level=None, time=None):
+def parameterise_rook(collection=None, area=None, level=None, time=None):
+    parameters = {}
+
+    parameters['area'] = area_parameter.AreaParameter(area).tuple
+    parameters['time'] = time_parameter.TimeParameter(time).tuple
+    parameters['level'] = level_parameter.LevelParameter(level).tuple
+    
+    return parameters 
+
+
+def parameterise_daops(collection=None, area=None, level=None, time=None):
     collection = collection_parameter.CollectionParameter(collection)
     area = area_parameter.AreaParameter(area)
     time = time_parameter.TimeParameter(time)
@@ -14,7 +24,7 @@ def parametrise_daops(collection=None, area=None, level=None, time=None):
     return collection.tuple, area.tuple, time.tuple, level.tuple
 
 
-def parametrise_clisops(area=None, level=None, time=None):
+def parameterise_clisops(area=None, level=None, time=None):
     area = area_parameter.AreaParameter(area)
     time = time_parameter.TimeParameter(time)
     level = level_parameter.LevelParameter(level)
