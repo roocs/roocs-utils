701ea9254002f76900ab48e4038f0e97f71c4649
Eleanor Smith <esmith88@sci5.jasmin.ac.uk>
2020-09-22 10:34:55 +0100

    working script to merge yaml files

--- roocs_utils/inventory/merge_yaml.py
+++ roocs_utils/inventory/merge_yaml.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import yaml
+import oyaml as yaml
 import os
 import glob
 
@@ -8,29 +8,25 @@ from roocs_utils.inventory.inventory import CustomDumper
 
 output_dir = "/gws/smf/j04/cp4cds1/c3s_34e/inventory"
 current_directory = os.getcwd()
-output_base = f"{output_dir}/c3s-cmip5_*.yml"
+output_base = f"{output_dir}/c3s-cordex_*.yml"
 
 yaml_files = glob.glob(output_base)
 
 
-def extend_dict(extend_me, extend_by):
-    if isinstance(extend_by, dict):
-        for k, v in extend_by.iteritems():
-            if k in extend_me:
-                extend_dict(extend_me.get(k), v)
-            else:
-                extend_me[k] = v
-    else:
-        extend_me += extend_by
+def write(new_yaml_data_dict):
+    if not os.path.isfile(yaml_files[0]):
+
+        with open(yaml_files[0], "a") as f:
+            f.write("---\n")
+
+    sdump = yaml.dump(new_yaml_data_dict, Dumper=CustomDumper)
+
+    with open(f"{output_dir}/c3s-cordex-merged.yml", "a") as f:
+        f.write(sdump)
 
 
-# Load the yaml files
 for i in range(1, len(yaml_files)-1):
-    extend_dict(yaml_files[0], yaml_files[i])
+    with open(yaml_files[i], 'r') as yamlfile1:
+        cmip5_update = yaml.load(yamlfile1, Loader=yaml.SafeLoader)
+    write(cmip5_update)
 
-
-print(yaml.dump(yaml_files[0], default_flow_style=False))
-
-# # create a new file with merged yaml
-# with open(f"{output_dir}/c3s-cmip5-merged.yml", 'w') as writer:
-#     yaml.dump(yaml_files[0], writer, Dumper=CustomDumper)
