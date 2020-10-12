d9d152b8fbb805c0c21948eb52714c47585583ec
agstephens <ag.stephens@stfc.ac.uk>
2020-10-12 17:04:07 +0100

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -96,6 +96,6 @@ setup(
     extras_require={"docs": docs_requirements},
     package_data={'roocs_utils': ['etc/roocs.ini']},
     url='https://github.com/roocs/roocs_utils',
-    version="0.1.0",
+    version="0.1.1",
     zip_safe=False,
 )
