5f8f1fea3ef17cfbf2a464f49b5b34b2103c3cb5
Eleanor Smith <esmith88@sci5.jasmin.ac.uk>
2020-10-16 11:39:35 +0100

    fixes for docs

--- roocs_utils/parameter/time_parameter.py
+++ roocs_utils/parameter/time_parameter.py
@@ -56,7 +56,7 @@ class TimeParameter(_BaseParameter):
     @property
     def tuple(self):
         """ Returns a tuple of the time values """
-        if self._parse_times() != (None, None):
+        if self._parse_times() is not (None, None):
             return self._parse_times()
 
     def asdict(self):
