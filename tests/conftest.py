5a66ee3b11865805f62c3506abcb3eab2435d577
Jonathan Haigh <jonathanhaigh0@gmail.com>
2021-02-15 00:36:30 +0000

    added branch value to lood_test_data fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -39,15 +39,18 @@ def load_test_data():
     This fixture ensures that the required test data repository
     has been cloned to the cache directory within the home directory.
     """
-    target = os.path.join(MINI_ESGF_CACHE_DIR, "master")
+    branch="master"
+    target = os.path.join(MINI_ESGF_CACHE_DIR, branch)
 
     if not os.path.isdir(MINI_ESGF_CACHE_DIR):
         os.makedirs(MINI_ESGF_CACHE_DIR)
     
     if not os.path.isdir(target):
-        Repo.clone_from(TEST_DATA_REPO_URL, target)
+        repo = Repo.clone_from(TEST_DATA_REPO_URL, target)
+        repo.git.checkout(branch)
 
     if not os.environ.get('ROOCS_AUTO_UPDATE_TEST_DATA', True) == 'FALSE':
         repo = Repo(target)
+        repo.git.checkout(branch)
         repo.remotes[0].pull()
 
