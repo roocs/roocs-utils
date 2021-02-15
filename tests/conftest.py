317c6e85ce7395a2e19e07d151ae20481c76c468
Jonathan Haigh <jonathanhaigh0@gmail.com>
2021-02-15 00:51:54 +0000

    fixed flake8 complaints

--- tests/conftest.py
+++ tests/conftest.py
@@ -39,12 +39,12 @@ def load_test_data():
     This fixture ensures that the required test data repository
     has been cloned to the cache directory within the home directory.
     """
-    branch="master"
+    branch = "master"
     target = os.path.join(MINI_ESGF_CACHE_DIR, branch)
 
     if not os.path.isdir(MINI_ESGF_CACHE_DIR):
         os.makedirs(MINI_ESGF_CACHE_DIR)
-    
+
     if not os.path.isdir(target):
         repo = Repo.clone_from(TEST_DATA_REPO_URL, target)
         repo.git.checkout(branch)
@@ -53,4 +53,3 @@ def load_test_data():
         repo = Repo(target)
         repo.git.checkout(branch)
         repo.remotes[0].pull()
-
