7e61ef993b151c2a215c69496107aa0108bd77ee
Carsten Ehbrecht <ehbrecht@dkrz.de>
2021-02-19 14:33:24 +0100

    Bump version: 0.2.0 → 0.2.1

--- docs/conf.py
+++ docs/conf.py
@@ -70,7 +70,7 @@ author = "Eleanor Smith"
 # the built documents.
 #
 # The short X.Y version.
-version = "0.2.0"
+version = "0.2.1"
 # The full version, including alpha/beta/rc tags.
 release = version
 
