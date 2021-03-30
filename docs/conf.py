27735fc4b6cd1a2e1cf5c7166326cf003eb714b7
ellesmith88 <e.s.smith@hotmail.co.uk>
2021-03-30 10:59:26 +0100

    Bump version: 0.2.1 → 0.3.0

--- docs/conf.py
+++ docs/conf.py
@@ -70,7 +70,7 @@ author = "Eleanor Smith"
 # the built documents.
 #
 # The short X.Y version.
-version = "0.2.1"
+version = "0.3.0"
 # The full version, including alpha/beta/rc tags.
 release = version
 
