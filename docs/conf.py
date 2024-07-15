6f1d9b1bf2d28c42b2fbea08f6ca48f8395148d5
Pingu Carsti <ehbrecht@dkrz.de>
2024-07-15 15:49:34 +0200

    Bump version: 0.6.8 → 0.6.9

--- docs/conf.py
+++ docs/conf.py
@@ -69,7 +69,7 @@ author = "Eleanor Smith"
 # the built documents.
 #
 # The short X.Y version.
-version = "0.6.8"
+version = "0.6.9"
 # The full version, including alpha/beta/rc tags.
 release = version
 
